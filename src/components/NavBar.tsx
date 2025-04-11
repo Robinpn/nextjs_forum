3cb5891315a40b905804013ad1962db0f16d5966
Robin Persson <robinpn98@hotmail.se>
2025-04-11 15:39:07 +0200

    MAde navbar responsive

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,19 +1,46 @@
+'use client';
 import Link from 'next/link';
 import React from 'react';
+import { Menu } from 'lucide-react';
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setIsOpen((prev) => !prev);
+  };
   return (
-    <div className="w-full h-10 border-b-2 sticky top-0 bg-black">
-      <ul className="flex justify-between mx-2">
-        <Link href={'/'}>Home</Link>
-        <Link href={'/create-post'}>Create</Link>
-        <Link href={'/posts'}>Posts</Link>
-        <Link href={'/private'} replace>
-          {/* replace is necessary for middleware to run */}
-          Account
-        </Link>
-      </ul>
-    </div>
+    <header className="w-full min-h-10 border-b-2 border-slate-800 fixed top-0 flex items-center justify-end">
+      <Menu
+        className=" z-20 md:hidden absolute top-2"
+        onClick={handleClick}
+        size={25}
+      />
+      <div className="hidden md:flex items-center justify-between flex-wrap w-full">
+        <ul className="flex justify-between mx-2 w-full">
+          <Link href={'/'}>Home</Link>
+          <Link href={'/create-post'}>Create</Link>
+          <Link href={'/posts'}>Posts</Link>
+          <Link href={'/private'} replace>
+            {/* replace is necessary for middleware to run */}
+            Account
+          </Link>
+        </ul>
+      </div>
+      {isOpen && (
+        <div className="flex flex-col basis-full  h-auto">
+          <ul className="flex flex-col justify-between items-center mx-2 gap-4">
+            <Link href={'/'}>Home</Link>
+            <Link href={'/create-post'}>Create</Link>
+            <Link href={'/posts'}>Posts</Link>
+            <Link href={'/private'} replace>
+              {/* replace is necessary for middleware to run */}
+              Account
+            </Link>
+          </ul>
+        </div>
+      )}
+    </header>
   );
 };
 
